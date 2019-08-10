7d5826998128ad9544df50bc5e691253ae44adc4
Taylor Gorman <me@taylorpatrickgorman.com>
2019-08-10 12:48:07 -0500

    📚 site details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
+
+const siteTitle = `Taylor Patrick Co.`
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: siteTitle,
+    author: `Taylor Gorman`,
+    description: `Website for taylorpatrick.co`,
+    siteUrl: `https://taylorpatrick.co`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `taylorgorman`,
     },
   },
   plugins: [
@@ -57,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: siteTitle,
+        short_name: siteTitle,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
